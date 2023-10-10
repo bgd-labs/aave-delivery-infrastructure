07827c6cd91a7b246a7e11f98c2e72e612d3d10e
sendra <polsendra@gmail.com>
2023-10-10 13:14:31 +0200

    fix: added mock destinations

--- scripts/helpers/Send_Direct_CCMessage.s.sol
+++ scripts/helpers/Send_Direct_CCMessage.s.sol
@@ -37,7 +37,7 @@ contract Ethereum is BaseSendDirectMessage {
   }
 
   function DESTINATION_NETWORK() public pure override returns (uint256) {
-    return ChainIds.GNOSIS;
+    return ChainIds.POLYGON;
   }
 }
 
