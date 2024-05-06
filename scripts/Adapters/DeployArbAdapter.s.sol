e1c8195b46cd80749c5bb9235c75f87a048d5170
sendra <polsendra@gmail.com>
2024-05-06 18:27:54 +0200

    fix: use lib to generate bytecode instead of deploying

--- scripts/Adapters/DeployArbAdapter.s.sol
+++ scripts/Adapters/DeployArbAdapter.s.sol
@@ -3,18 +3,27 @@ pragma solidity ^0.8.0;
 
 import {ArbAdapter, IArbAdapter} from '../../src/contracts/adapters/arbitrum/ArbAdapter.sol';
 import {ArbitrumAdapterTestnet} from '../contract_extensions/ArbitrumAdapter.sol';
-import {IBaseAdapterScript} from './IBaseAdapterScript.sol';
 
-contract BaseArbAdapter is IBaseAdapterScript {
-  function _deployAdapter(
-    IBaseAdapterScript.BaseAdapterArgs memory baseArgs,
+//import {IBaseAdapterScript} from './IBaseAdapterScript.sol';
+
+library ArbAdapterDeploymentHelper {
+  struct BaseAdapterArgs {
+    address crossChainController;
+    uint256 providerGasLimit;
+    IBaseAdapter.TrustedRemotesConfig[] trustedRemotes;
+    bool isTestnet;
+  }
+
+  function getAdapterCode(
+    BaseAdapterArgs memory baseArgs,
     address inbox,
     address destinationCCC
-  ) internal returns (address) {
+  ) internal pure returns (bytes memory) {
     if (baseArgs.isTestnet) {
       return
-        address(
-          new ArbitrumAdapterTestnet{salt: baseArgs.adapterSalt}(
+        abi.encodePacked(
+          type(ArbitrumAdapterTestnet).creationCode,
+          abi.encode(
             baseArgs.crossChainController,
             inbox,
             destinationCCC,
@@ -24,8 +33,9 @@ contract BaseArbAdapter is IBaseAdapterScript {
         );
     } else {
       return
-        address(
-          new ArbAdapter{salt: baseArgs.adapterSalt}(
+        abi.encodePacked(
+          type(ArbAdapter).creationCode,
+          abi.encode(
             baseArgs.crossChainController,
             inbox,
             destinationCCC,
@@ -35,4 +45,34 @@ contract BaseArbAdapter is IBaseAdapterScript {
         );
     }
   }
+
+  //  function _deployAdapter(
+  //    IBaseAdapterScript.BaseAdapterArgs memory baseArgs,
+  //    address inbox,
+  //    address destinationCCC
+  //  ) internal returns (address) {
+  //    if (baseArgs.isTestnet) {
+  //      return
+  //        address(
+  //          new ArbitrumAdapterTestnet{salt: baseArgs.adapterSalt}(
+  //            baseArgs.crossChainController,
+  //            inbox,
+  //            destinationCCC,
+  //            baseArgs.providerGasLimit,
+  //            baseArgs.trustedRemotes
+  //          )
+  //        );
+  //    } else {
+  //      return
+  //        address(
+  //          new ArbAdapter{salt: baseArgs.adapterSalt}(
+  //            baseArgs.crossChainController,
+  //            inbox,
+  //            destinationCCC,
+  //            baseArgs.providerGasLimit,
+  //            baseArgs.trustedRemotes
+  //          )
+  //        );
+  //    }
+  //  }
 }
