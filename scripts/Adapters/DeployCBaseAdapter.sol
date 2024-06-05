21eb62c64cec9c7275b80b9446284ba572082549
sendra <polsendra@gmail.com>
2024-06-05 15:05:58 +0200

    feat: added CB and CCIP adapters

---
+++ scripts/Adapters/DeployCBaseAdapter.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import {CBaseAdapter, IBaseAdapter} from '../../src/contracts/adapters/cBase/CBaseAdapter.sol';
+import './BaseAdapterScript.sol';
+import {CBaseAdapterTestnet} from '../contract_extensions/CBAdapter.sol';
+
+library CBAdapterDeploymentHelper {
+  struct CBAdapterArgs {
+    BaseAdapterArgs baseArgs;
+    address ovm;
+  }
+
+  function getAdapterCode(CBAdapterArgs memory cbArgs) internal pure returns (bytes memory) {
+    bytes memory creationCode = cbArgs.baseArgs.isTestnet
+      ? type(CBaseAdapterTestnet).creationCode
+      : type(CBaseAdapter).creationCode;
+
+    return
+      abi.encodePacked(
+        creationCode,
+        abi.encode(
+          cbArgs.baseArgs.crossChainController,
+          cbArgs.ovm,
+          cbArgs.baseArgs.providerGasLimit,
+          cbArgs.baseArgs.trustedRemotes
+        )
+      );
+  }
+}
+
+abstract contract BaseCBAdapter is BaseAdapterScript {
+  function OVM() public view virtual returns (address);
+
+  function _getAdapterByteCode(
+    address currentNetworkCCC,
+    IBaseAdapter.TrustedRemotesConfig[] memory trustedRemotes
+  ) internal view override returns (bytes memory) {
+    require(OVM() != address(0), 'Invalid OVM address');
+    require(currentNetworkCCC != address(0), 'CCC needs to be deployed');
+    require(trustedRemotes.length == 1, 'Adapter can only have one remote');
+
+    CBAdapterDeploymentHelper.CBAdapterArgs memory constructorArgs = CBAdapterDeploymentHelper
+      .CBAdapterArgs({
+        baseArgs: BaseAdapterArgs({
+          crossChainController: currentNetworkCCC,
+          providerGasLimit: PROVIDER_GAS_LIMIT(),
+          trustedRemotes: trustedRemotes,
+          isTestnet: isTestnet()
+        }),
+        ovm: OVM()
+      });
+
+    return CBAdapterDeploymentHelper.getAdapterCode(constructorArgs);
+  }
+}
---
+++ scripts/Adapters/DeployCCIP.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import {CCIPAdapter, ICCIPAdapter, IBaseAdapter} from '../../src/contracts/adapters/ccip/CCIPAdapter.sol';
+import './BaseAdapterScript.sol';
+import {CCIPAdapterTestnet} from '../contract_extensions/CCIPAdapter.sol';
+
+library CCIPAdapterDeploymentHelper {
+  struct CCIPAdapterArgs {
+    BaseAdapterArgs baseArgs;
+    address ccipRouter;
+    address linkToken;
+  }
+
+  function getAdapterCode(CCIPAdapterArgs memory ccipArgs) internal pure returns (bytes memory) {
+    bytes memory creationCode = ccipArgs.baseArgs.isTestnet
+      ? type(CCIPAdapterTestnet).creationCode
+      : type(CCIPAdapter).creationCode;
+
+    return
+      abi.encodePacked(
+        creationCode,
+        abi.encode(
+          ccipArgs.baseArgs.crossChainController,
+          ccipArgs.ccipRouter,
+          ccipArgs.baseArgs.providerGasLimit,
+          ccipArgs.baseArgs.trustedRemotes,
+          ccipArgs.linkToken
+        )
+      );
+  }
+}
+
+// configs can be found here: https://docs.chain.link/ccip/supported-networks/v1_2_0/mainnet#bnb-mainnet
+abstract contract BaseCCIPAdapter is BaseAdapterScript {
+  function CCIP_ROUTER() public view virtual returns (address);
+
+  function LINK_TOKEN() public view virtual returns (address);
+
+  function _getAdapterByteCode(
+    address currentNetworkCCC,
+    IBaseAdapter.TrustedRemotesConfig[] memory trustedRemotes
+  ) internal override returns (bytes memory) {
+    require(currentNetworkCCC != address(0), 'CCC needs to be deployed');
+    require(CCIP_ROUTER() != address(0), 'Invalid CCIP Router');
+    require(LINK_TOKEN() != address(0), 'Invalid Link Token');
+
+    CCIPAdapterDeploymentHelper.CCIPAdapterArgs memory constructorArgs = CCIPAdapterDeploymentHelper
+      .CCIPAdapterArgs({
+        baseArgs: BaseAdapterArgs({
+          crossChainController: currentNetworkCCC,
+          providerGasLimit: PROVIDER_GAS_LIMIT(),
+          trustedRemotes: trustedRemotes,
+          isTestnet: isTestnet()
+        }),
+        ccipRouter: CCIP_ROUTER(),
+        linkToken: LINK_TOKEN()
+      });
+
+    return CCIPAdapterDeploymentHelper.getAdapterCode(constructorArgs);
+  }
+}
