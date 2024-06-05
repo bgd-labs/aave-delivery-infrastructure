b0d34f40324d414a4198972f47818e2425aacb51
sendra <polsendra@gmail.com>
2024-06-05 12:26:33 +0200

    WIP: cb adapter

--- scripts/Adapters/DeployCBaseAdapter.s.sol
+++ scripts/Adapters/DeployCBaseAdapter.s.sol
@@ -8,14 +8,12 @@ import {CBaseAdapterTestnet} from '../contract_extensions/CBAdapter.sol';
 abstract contract BaseCBAdapter is BaseAdapterScript {
   function OVM() public view virtual returns (address);
 
-  function isTestnet() public view virtual returns (bool) {
-    return false;
-  }
-
   function _deployAdapter(
     DeployerHelpers.Addresses memory addresses,
     IBaseAdapter.TrustedRemotesConfig[] memory trustedRemotes
   ) internal override {
+    require(OVM() != address(0), 'Invalid OVM address');
+
     if (isTestnet()) {
       addresses.baseAdapter = address(
         new CBaseAdapterTestnet(
@@ -35,79 +33,5 @@ abstract contract BaseCBAdapter is BaseAdapterScript {
         )
       );
     }
-  }
-}
-
-contract Ethereum is BaseCBAdapter {
-  function OVM() public pure override returns (address) {
-    return 0x866E82a600A1414e583f7F13623F1aC5d58b0Afa;
-  }
-
-  function TRANSACTION_NETWORK() public pure override returns (uint256) {
-    return ChainIds.ETHEREUM;
-  }
-
-  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
-    uint256[] memory remoteNetworks = new uint256[](0);
-
-    return remoteNetworks;
-  }
-}
-
-contract Base is BaseCBAdapter {
-  function OVM() public pure override returns (address) {
-    return 0x4200000000000000000000000000000000000007;
-  }
-
-  function TRANSACTION_NETWORK() public pure override returns (uint256) {
-    return ChainIds.BASE;
-  }
-
-  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
-    uint256[] memory remoteNetworks = new uint256[](1);
-    remoteNetworks[0] = ChainIds.ETHEREUM;
-
-    return remoteNetworks;
-  }
-}
-
-contract Ethereum_testnet is BaseCBAdapter {
-  function OVM() public pure override returns (address) {
-    return 0x8e5693140eA606bcEB98761d9beB1BC87383706D;
-  }
-
-  function isTestnet() public pure override returns (bool) {
-    return true;
-  }
-
-  function TRANSACTION_NETWORK() public pure override returns (uint256) {
-    return TestNetChainIds.ETHEREUM_GOERLI;
-  }
-
-  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
-    uint256[] memory remoteNetworks = new uint256[](0);
-
-    return remoteNetworks;
-  }
-}
-
-contract Base_testnet is BaseCBAdapter {
-  function OVM() public pure override returns (address) {
-    return 0x4200000000000000000000000000000000000007;
-  }
-
-  function isTestnet() public pure override returns (bool) {
-    return true;
-  }
-
-  function TRANSACTION_NETWORK() public pure override returns (uint256) {
-    return TestNetChainIds.BASE_GOERLI;
-  }
-
-  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
-    uint256[] memory remoteNetworks = new uint256[](1);
-    remoteNetworks[0] = TestNetChainIds.ETHEREUM_GOERLI;
-
-    return remoteNetworks;
   }
 }
