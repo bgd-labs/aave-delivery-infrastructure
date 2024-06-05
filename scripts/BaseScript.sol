a9edcba3a2c4b193a992a48238839f2a79b32713
sendra <polsendra@gmail.com>
2024-06-05 11:55:19 +0200

    fix: remove Script inheritance

--- scripts/BaseScript.sol
+++ scripts/BaseScript.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import 'forge-std/Script.sol';
-import 'forge-std/Vm.sol';
 import {ChainIds, TestNetChainIds} from 'aave-helpers/ChainIds.sol';
 import {Create2Utils} from 'aave-helpers/ScriptUtils.sol';
 
-abstract contract BaseScript is Script {
+abstract contract BaseScript {
   function TRANSACTION_NETWORK() internal view virtual returns (uint256);
 }
