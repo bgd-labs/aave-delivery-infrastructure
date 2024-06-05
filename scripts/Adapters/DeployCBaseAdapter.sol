3d2ace12c974e7539cde8b466b5af71bea42b13e
sendra <polsendra@gmail.com>
2024-06-05 11:41:06 +0200

    fix: revert back not yet needed adapter scripts

--- scripts/Adapters/DeployCBaseAdapter.sol
+++ scripts/Adapters/DeployCBaseAdapter.sol
@@ -1,39 +1,113 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {CBaseAdapter} from '../../src/contracts/adapters/cBase/CBaseAdapter.sol';
+import {CBaseAdapter, IBaseAdapter} from '../../src/contracts/adapters/cBase/CBaseAdapter.sol';
+import './BaseAdapterScript.sol';
 import {CBaseAdapterTestnet} from '../contract_extensions/CBAdapter.sol';
-import './BaseAdapterStructs.sol';
 
-library BaseCBAdapter {
-  struct CBAdapterArgs {
-    BaseAdapterStructs.BaseAdapterArgs baseArgs;
-    address ovm;
+abstract contract BaseCBAdapter is BaseAdapterScript {
+  function OVM() public view virtual returns (address);
+
+  function isTestnet() public view virtual returns (bool) {
+    return false;
   }
 
-  function getAdapterCode(CBAdapterArgs memory cbArgs) internal pure returns (bytes memory) {
-    if (cbArgs.baseArgs.isTestnet) {
-      return
-        abi.encodePacked(
-          type(CBaseAdapterTestnet).creationCode,
-          abi.encode(
-            cbArgs.baseArgs.crossChainController,
-            cbArgs.ovm,
-            cbArgs.baseArgs.providerGasLimit,
-            cbArgs.baseArgs.trustedRemotes
-          )
-        );
+  function _deployAdapter(
+    DeployerHelpers.Addresses memory addresses,
+    IBaseAdapter.TrustedRemotesConfig[] memory trustedRemotes
+  ) internal override {
+    if (isTestnet()) {
+      addresses.baseAdapter = address(
+        new CBaseAdapterTestnet(
+          addresses.crossChainController,
+          OVM(),
+          GET_BASE_GAS_LIMIT(),
+          trustedRemotes
+        )
+      );
     } else {
-      return
-        abi.encodePacked(
-          type(CBaseAdapter).creationCode,
-          abi.encode(
-            cbArgs.baseArgs.crossChainController,
-            cbArgs.ovm,
-            cbArgs.baseArgs.providerGasLimit,
-            cbArgs.baseArgs.trustedRemotes
-          )
-        );
+      addresses.baseAdapter = address(
+        new CBaseAdapter(
+          addresses.crossChainController,
+          OVM(),
+          GET_BASE_GAS_LIMIT(),
+          trustedRemotes
+        )
+      );
     }
+  }
+}
+
+contract Ethereum is BaseCBAdapter {
+  function OVM() public pure override returns (address) {
+    return 0x866E82a600A1414e583f7F13623F1aC5d58b0Afa;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](0);
+
+    return remoteNetworks;
+  }
+}
+
+contract Base is BaseCBAdapter {
+  function OVM() public pure override returns (address) {
+    return 0x4200000000000000000000000000000000000007;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.BASE;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+}
+
+contract Ethereum_testnet is BaseCBAdapter {
+  function OVM() public pure override returns (address) {
+    return 0x8e5693140eA606bcEB98761d9beB1BC87383706D;
+  }
+
+  function isTestnet() public pure override returns (bool) {
+    return true;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_GOERLI;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](0);
+
+    return remoteNetworks;
+  }
+}
+
+contract Base_testnet is BaseCBAdapter {
+  function OVM() public pure override returns (address) {
+    return 0x4200000000000000000000000000000000000007;
+  }
+
+  function isTestnet() public pure override returns (bool) {
+    return true;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.BASE_GOERLI;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_GOERLI;
+
+    return remoteNetworks;
   }
 }
--- scripts/Adapters/DeployLZ.s.sol
+++ scripts/Adapters/DeployLZ.s.sol
@@ -1,36 +1,294 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {LayerZeroAdapter, ILayerZeroAdapter} from '../../src/contracts/adapters/layerZero/LayerZeroAdapter.sol';
+import {LayerZeroAdapter, ILayerZeroAdapter, IBaseAdapter} from '../../src/contracts/adapters/layerZero/LayerZeroAdapter.sol';
 import {LayerZeroAdapterTestnet} from '../contract_extensions/LayerZeroAdapter.sol';
-import {IBaseAdapterScript} from './BaseAdapterStructs.sol';
+import '../BaseScript.sol';
+import './BaseAdapterScript.sol';
 
-abstract contract BaseLZAdapter is IBaseAdapterScript {
+abstract contract BaseLZAdapter is BaseAdapterScript {
+  function LZ_ENDPOINT() public view virtual returns (address);
+
+  function isTestNet() public view virtual returns (bool);
+
   function _deployAdapter(
-    IBaseAdapterScript.BaseAdapterArgs memory baseArgs,
-    address lzEndpoint
-  ) internal returns (address) {
+    DeployerHelpers.Addresses memory addresses,
+    IBaseAdapter.TrustedRemotesConfig[] memory trustedRemotes
+  ) internal override {
     address lzAdapter;
-    if (baseArgs.isTestnet) {
-      return
-        address(
-          new LayerZeroAdapterTestnet{salt: baseArgs.adapterSalt}(
-            lzEndpoint,
-            baseArgs.crossChainController,
-            baseArgs.providerGasLimit,
-            baseArgs.trustedRemotes
-          )
-        );
+    if (isTestNet()) {
+      lzAdapter = address(
+        new LayerZeroAdapterTestnet(
+          LZ_ENDPOINT(),
+          addresses.crossChainController,
+          GET_BASE_GAS_LIMIT(),
+          trustedRemotes
+        )
+      );
     } else {
-      return
-        address(
-          new LayerZeroAdapter{salt: baseArgs.adapterSalt}(
-            lzEndpoint,
-            baseArgs.crossChainController,
-            baseArgs.providerGasLimit,
-            baseArgs.trustedRemotes
-          )
-        );
+      lzAdapter = address(
+        new LayerZeroAdapter(
+          LZ_ENDPOINT(),
+          addresses.crossChainController,
+          GET_BASE_GAS_LIMIT(),
+          trustedRemotes
+        )
+      );
     }
+    addresses.lzAdapter = lzAdapter;
+  }
+}
+
+contract Ethereum is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](2);
+    remoteNetworks[0] = ChainIds.POLYGON;
+    remoteNetworks[1] = ChainIds.AVALANCHE;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Ethereum_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_SEPOLIA;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](2);
+    remoteNetworks[0] = TestNetChainIds.POLYGON_MUMBAI;
+    remoteNetworks[1] = TestNetChainIds.AVALANCHE_FUJI;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
+  }
+}
+
+contract Avalanche is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.AVALANCHE;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Avalanche_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.AVALANCHE_FUJI;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_SEPOLIA;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
+  }
+}
+
+contract Polygon is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.POLYGON;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Polygon_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.POLYGON_MUMBAI;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_SEPOLIA;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
+  }
+}
+
+contract Binance is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.BNB;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Binance_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.BNB_TESTNET;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_SEPOLIA;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
+  }
+}
+
+contract Gnosis is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.GNOSIS;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Gnosis_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.GNOSIS_CHIADO;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_GOERLI;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
+  }
+}
+
+contract Celo is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x1a44076050125825900e736c501f859c50fE728c;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.CELO;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = ChainIds.ETHEREUM;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return false;
+  }
+}
+
+contract Celo_testnet is BaseLZAdapter {
+  function LZ_ENDPOINT() public pure override returns (address) {
+    return 0x6EDCE65403992e310A62460808c4b910D972f10f;
+  }
+
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.CELO_ALFAJORES;
+  }
+
+  function REMOTE_NETWORKS() public pure override returns (uint256[] memory) {
+    uint256[] memory remoteNetworks = new uint256[](1);
+    remoteNetworks[0] = TestNetChainIds.ETHEREUM_SEPOLIA;
+
+    return remoteNetworks;
+  }
+
+  function isTestNet() public pure override returns (bool) {
+    return true;
   }
 }
