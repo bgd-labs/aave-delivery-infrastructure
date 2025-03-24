e17776d2fe556b6b02d55ff0c6dca79a5b6dc11f
sendra <polsendra@gmail.com>
2025-03-24 16:17:03 +0100

    feat: Add Ink native adapter

---
+++ src/contracts/adapters/ink/InkAdapter.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import {BaseAdapter, IBaseAdapter} from '../BaseAdapter.sol';
+import {Errors} from '../../libs/Errors.sol';
+import {ChainIds} from 'solidity-utils/contracts/utils/ChainHelpers.sol';
+import {OpAdapter, IOpAdapter} from '../optimism/OpAdapter.sol';
+
+/**
+ * @title InkAdapter
+ * @author BGD Labs
+ * @notice Ink bridge adapter. Used to send and receive messages cross chain between Ethereum and Ink
+ * @dev it uses the eth balance of CrossChainController contract to pay for message bridging as the method to bridge
+        is called via delegate call
+ * @dev note that this adapter can only be used for the communication path ETHEREUM -> INK
+ * @dev note that this adapter inherits from Optimism adapter and overrides only supported chain
+ */
+contract InkAdapter is OpAdapter {
+  /**
+   * @param crossChainController address of the cross chain controller that will use this bridge adapter
+   * @param ovmCrossDomainMessenger ink entry point address
+   * @param providerGasLimit base gas limit used by the bridge adapter
+   * @param trustedRemotes list of remote configurations to set as trusted
+   */
+  constructor(
+    address crossChainController,
+    address ovmCrossDomainMessenger,
+    uint256 providerGasLimit,
+    TrustedRemotesConfig[] memory trustedRemotes
+  )
+    OpAdapter(
+      crossChainController,
+      ovmCrossDomainMessenger,
+      providerGasLimit,
+      'Ink native adapter',
+      trustedRemotes
+    )
+  {}
+
+  /// @inheritdoc IOpAdapter
+  function isDestinationChainIdSupported(
+    uint256 chainId
+  ) public view virtual override returns (bool) {
+    return chainId == ChainIds.INK;
+  }
+
+  /// @inheritdoc IOpAdapter
+  function getOriginChainId() public pure virtual override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+}
